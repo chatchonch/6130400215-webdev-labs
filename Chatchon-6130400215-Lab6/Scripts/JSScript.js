193263ce425d824eda3b0e69986c3dd88aed6734
chatchonch <68958067+chatchonch@users.noreply.github.com>
2020-09-16 17:35:58 +0700

    Add files via upload

--- Chatchon-6130400215-Lab6/Scripts/JSScript.js
+++ Chatchon-6130400215-Lab6/Scripts/JSScript.js
@@ -1,9 +1,13 @@
 outter : while (true) {
 	var input = prompt('Prime Number Finder. Put your positive integer.');
-	var number = Number(input);
+	var numinteger = parseInt(input);
+	var	numfloat = parseFloat(input);
+	var	number = Number(input);
 	var flag = false;
-	if (number == 1) {
+	if  (numinteger-numfloat !== 0) {
 		alert('1 is not a prime number.');
+	} else if  {
+		alert('Enter a positive integer!');
 	} else if (number == 2) {
 		alert('2 is a prime number.')
 		break outter;
@@ -22,4 +26,5 @@ inner : 	for (let i = 2; i < number;i++) {
 	} else {
 		alert('Enter a positive integer!');
 	}
+	
 }
