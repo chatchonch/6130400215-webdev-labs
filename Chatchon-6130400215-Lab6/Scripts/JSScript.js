62e315328d7ca87b948cae9780e33e5dc74bcd96
chatchonch <68958067+chatchonch@users.noreply.github.com>
2020-09-16 16:39:26 +0700

    Lab 6
    First Problem

---
+++ Chatchon-6130400215-Lab6/Scripts/JSScript.js
@@ -0,0 +1,25 @@
+outter : while (true) {
+	var input = prompt('Prime Number Finder. Put your positive integer.');
+	var number = Number(input);
+	var flag = false;
+	if (number == 1) {
+		alert('1 is not a prime number.');
+	} else if (number == 2) {
+		alert('2 is a prime number.')
+		break outter;
+	} else if (number > 1){
+		var result = [];
+inner : 	for (let i = 2; i < number;i++) {
+				for (let j = 2;j < i;j++) {
+					if (i % j == 0) {
+						continue inner;
+					}
+				}
+				result.push(i);
+			}
+			alert('For n = '+ number + ' prime numbers are ' + result);
+			break outter;
+	} else {
+		alert('Enter a positive integer!');
+	}
+}
